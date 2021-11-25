bf435106f2d7d06a69de49a54deff1f5b00f7e0c
pablotrek <pablo.d.guzman.1@gmail.com>
2021-11-25 13:36:24 -0300

    agregado de tablas secundarias con FK

--- database/structure.sql
+++ database/structure.sql
@@ -16,6 +16,41 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `cart`
+--
+
+DROP TABLE IF EXISTS `cart`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cart` (
+  `id_cart` int NOT NULL AUTO_INCREMENT,
+  `fk_id_user` int NOT NULL,
+  `fk_id_product` int NOT NULL,
+  `fk_id_order` int NOT NULL,
+  `cant` int NOT NULL DEFAULT '0',
+  `fk_product_price` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`id_cart`),
+  UNIQUE KEY `id_cart_UNIQUE` (`id_cart`),
+  KEY `FK_id_user_idx` (`fk_id_user`),
+  KEY `FK_product_idx` (`fk_id_product`),
+  KEY `FK_id_order_idx` (`fk_id_order`),
+  KEY `FK_id_price_idx` (`fk_product_price`),
+  CONSTRAINT `FK_id_order` FOREIGN KEY (`fk_id_order`) REFERENCES `purchase_order` (`id_purchase_order`),
+  CONSTRAINT `FK_id_product` FOREIGN KEY (`fk_id_product`) REFERENCES `products` (`id_product`),
+  CONSTRAINT `FK_id_user` FOREIGN KEY (`fk_id_user`) REFERENCES `users` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cart`
+--
+
+LOCK TABLES `cart` WRITE;
+/*!40000 ALTER TABLE `cart` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cart` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `categories`
 --
 
@@ -23,10 +58,10 @@ DROP TABLE IF EXISTS `categories`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `categories` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id_category` int NOT NULL AUTO_INCREMENT,
   `name` varchar(25) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
+  PRIMARY KEY (`id_category`),
+  UNIQUE KEY `id_UNIQUE` (`id_category`),
   UNIQUE KEY `name_UNIQUE` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -48,10 +83,10 @@ DROP TABLE IF EXISTS `color`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `color` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id_color` int NOT NULL AUTO_INCREMENT,
   `name` varchar(25) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
+  PRIMARY KEY (`id_color`),
+  UNIQUE KEY `id_UNIQUE` (`id_color`),
   UNIQUE KEY `name_UNIQUE` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -73,11 +108,11 @@ DROP TABLE IF EXISTS `genre`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `genre` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id_genre` int NOT NULL AUTO_INCREMENT,
   `name` varchar(25) NOT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`id_genre`),
   UNIQUE KEY `category_UNIQUE` (`name`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
+  UNIQUE KEY `id_UNIQUE` (`id_genre`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -91,28 +126,28 @@ LOCK TABLES `genre` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `marc`
+-- Table structure for table `marks`
 --
 
-DROP TABLE IF EXISTS `marc`;
+DROP TABLE IF EXISTS `marks`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `marc` (
-  `id` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `marks` (
+  `id_mark` int NOT NULL AUTO_INCREMENT,
   `name` varchar(25) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
+  PRIMARY KEY (`id_mark`),
+  UNIQUE KEY `id_UNIQUE` (`id_mark`),
   UNIQUE KEY `name_UNIQUE` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `marc`
+-- Dumping data for table `marks`
 --
 
-LOCK TABLES `marc` WRITE;
-/*!40000 ALTER TABLE `marc` DISABLE KEYS */;
-/*!40000 ALTER TABLE `marc` ENABLE KEYS */;
+LOCK TABLES `marks` WRITE;
+/*!40000 ALTER TABLE `marks` DISABLE KEYS */;
+/*!40000 ALTER TABLE `marks` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -123,15 +158,25 @@ DROP TABLE IF EXISTS `products`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `products` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id_product` int NOT NULL AUTO_INCREMENT,
   `name_product` varchar(45) NOT NULL,
   `description_product` text NOT NULL,
   `product_image` varchar(25) NOT NULL,
-  `price` decimal(8,2) NOT NULL,
-  `user_id` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `product_id_UNIQUE` (`id`),
-  UNIQUE KEY `user_id_UNIQUE` (`user_id`)
+  `price` decimal(10,2) NOT NULL,
+  `fk_color` int DEFAULT NULL,
+  `fk_category` int DEFAULT NULL,
+  `fk_mark` int DEFAULT NULL,
+  `fk_genre` int DEFAULT NULL,
+  PRIMARY KEY (`id_product`),
+  UNIQUE KEY `product_id_UNIQUE` (`id_product`),
+  KEY `FK_id_color_idx` (`fk_color`),
+  KEY `FK_id_category_idx` (`fk_category`),
+  KEY `FK_id_genre_idx` (`fk_genre`),
+  KEY `FK_id_mark_idx` (`fk_mark`),
+  CONSTRAINT `FK_id_category` FOREIGN KEY (`fk_category`) REFERENCES `categories` (`id_category`),
+  CONSTRAINT `FK_id_color` FOREIGN KEY (`fk_color`) REFERENCES `color` (`id_color`),
+  CONSTRAINT `FK_id_genre` FOREIGN KEY (`fk_genre`) REFERENCES `genre` (`id_genre`),
+  CONSTRAINT `FK_id_mark` FOREIGN KEY (`fk_mark`) REFERENCES `marks` (`id_mark`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -145,6 +190,34 @@ LOCK TABLES `products` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `purchase_order`
+--
+
+DROP TABLE IF EXISTS `purchase_order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `purchase_order` (
+  `id_purchase_order` int NOT NULL AUTO_INCREMENT,
+  `fk_id_user` int NOT NULL,
+  `shipping` int NOT NULL,
+  `payment_method` varchar(15) NOT NULL DEFAULT 'credit_card',
+  PRIMARY KEY (`id_purchase_order`),
+  UNIQUE KEY `id_UNIQUE` (`id_purchase_order`),
+  KEY `FK_user_idx` (`fk_id_user`),
+  CONSTRAINT `FK_user` FOREIGN KEY (`fk_id_user`) REFERENCES `users` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `purchase_order`
+--
+
+LOCK TABLES `purchase_order` WRITE;
+/*!40000 ALTER TABLE `purchase_order` DISABLE KEYS */;
+/*!40000 ALTER TABLE `purchase_order` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `users`
 --
 
@@ -152,13 +225,13 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id_user` int NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `email` varchar(85) NOT NULL,
   `password` varchar(20) NOT NULL,
   `image` varchar(25) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
+  PRIMARY KEY (`id_user`),
+  UNIQUE KEY `id_UNIQUE` (`id_user`),
   UNIQUE KEY `email_UNIQUE` (`email`),
   UNIQUE KEY `password_UNIQUE` (`password`),
   UNIQUE KEY `image_UNIQUE` (`image`)
@@ -183,4 +256,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-11-18 20:59:05
+-- Dump completed on 2021-11-25 13:31:31
