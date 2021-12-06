0c050c825951588b5c441dcacf795a75e52d1238
PABLOTREK <pablo.d.guzman.1@gmail.com>
2021-12-06 14:38:30 -0300

    nuevamente agregado desde vsc de archivo borrado en github

---
+++ database/models/Trademark.js
@@ -0,0 +1,28 @@
+module.exports = function (sequelize, dataTypes) {
+    let alias = 'TrademarksTable';
+  
+    let cols = {
+      id_trademark: {
+        type: dataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        notNull: true,
+      },
+      name: {
+        type: dataTypes.STRING,
+        notNull: true,
+      },
+    };
+  
+    let config = {
+      tableName: 'trademark',
+      timestamps: false,
+    };
+  
+    let Trademarks = sequelize.define(alias, cols, config);
+  
+    Trademarks.associate = function (models) {
+      Trademarks.hasMany(models.ProductsTable);
+    };
+    return Trademarks;
+  };
