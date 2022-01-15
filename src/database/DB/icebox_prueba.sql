64f3723c0bdc5f6b46c45464995e9cff0c3d3a19
LowOnCYAN23 <trikerdg@gmail.com>
2022-01-14 21:24:09 -0500

    Se agrego el archivo icebox_prueba en la carpeta DB

---
+++ src/database/DB/icebox_prueba.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: icebox_prueba
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id_users` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(85) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `image` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_users`),
+  UNIQUE KEY `id_users_UNIQUE` (`id_users`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  UNIQUE KEY `password_UNIQUE` (`password`)
+) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Diego Martínez','diego123@hotmail.com','hola123','default.png'),(2,'Luna Gatica','luna123@hotmail.com','luna123','default.png'),(3,'Facu Maldonado','facu123@hotmail.com','facu123','default.png'),(4,'Pablo Cruz','pablo123@hotmail.com','pablo123','default.png'),(5,'Pepito Peréz','pepito123@hotmail.com','pepito123','default.png'),(6,'George Washington','usa123@hotmail.com','usa123','default.png'),(7,'Albert Einstein','relativo123@hotmail.com','mc123','default.png'),(8,'Eminem','eminem123@hotmail.com','rap123','default.png'),(9,'Freddie Mercury','queen123@hotmail.com','queen123','default.png'),(10,'Michael Jackson','beat123@hotmail.com','MJ123','default.png'),(51,'Elmo Jones','elmo@hotmail.com','elmo123','');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-01-14 21:22:27
