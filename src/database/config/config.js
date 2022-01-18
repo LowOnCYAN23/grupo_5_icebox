2a016e3d53ec89e0e7f0e2c4e227c3a46236bb6d
PABLO <pablo.d.guzman.1@gmail.com>
2022-01-18 13:58:08 -0300

    correccion en register.ejs

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     username: 'root',
-    password: 'sashasan23',
+    password: 'Bl@sasfda',
     database: 'icebox_prueba',
     host: '127.0.0.1',
     dialect: 'mysql',
